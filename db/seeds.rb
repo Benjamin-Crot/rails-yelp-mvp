664763170838f2fb49e76067389f2d6176033b53
Benjamin Crot <benjamin.crot@gmail.com>
2020-02-12 14:53:13 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bristol = Restaurant.create(name: "Bristol", category: "french", address: "15 rue Montcalm")
+alamesa = Restaurant.create(name: "A la mesa", category: "french", address: "92 rue des Dames")
+mastino = Restaurant.create(name: "Mastino", category: "italian", address: "15 rue de l'Etoile")
+surpriz = Restaurant.create(name: "Surpriz", category: "belgian", address: "12 rue Oberkamptf")
+sushishop = Restaurant.create(name: "Sushi Shop", category: "japanese", address: "29 rue Tocqueville")
+
